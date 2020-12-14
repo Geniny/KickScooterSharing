af476a907d8231b7bf3d24756b7bc24aac07626c
Geniny <nonamebylka@mail.ru>
2020-12-14 21:50:17 +0300

    add: user managaring

--- KickScooterSharing/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ KickScooterSharing/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -23,18 +23,14 @@ namespace KickScooterSharing.Areas.Identity.Pages.Account
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
-
         public RegisterModel(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -75,33 +71,14 @@ namespace KickScooterSharing.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email , StatusId = 1};
+                var user = new User { UserName = Input.Email, Email = Input.Email , StatusId = 1, FirstNameId = 4, SecondNameId = 3};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
                     
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    /*
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
                     
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                    */
                         await _signInManager.SignInAsync(user, isPersistent: false);
-                        result = await _userManager.ConfirmEmailAsync(user, code);
                     return LocalRedirect(returnUrl);
                     //}
                 }

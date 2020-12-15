51794bb6a1b698c9a6b26f6d5e40d0b28fcaf526
Beelzebub <sanchesik1111@gmail.com>
2020-12-15 03:13:14 +0300

    add : product add/edit

--- KickScooterSharing/Controllers/ProductsController.cs
+++ KickScooterSharing/Controllers/ProductsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using KickScooterSharing.Data;
 using KickScooterSharing.Models;
 using Microsoft.AspNetCore.Authorization;
+using KickScooterSharing.ViewModels;
 
 namespace KickScooterSharing.Controllers
 {
@@ -52,26 +53,38 @@ namespace KickScooterSharing.Controllers
         // GET: Products/Create
         public IActionResult Create()
         {
-            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Id");
-            ViewData["StatusId"] = new SelectList(_context.Set<Status>(), "Id", "Id");
-            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Id");
+            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Number");
+            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Name");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,TariffId,ScooterId,StatusId")] Product product)
+        public async Task<IActionResult> Create([Bind("Id,Name,TariffId,ScooterId,Latitude,Longitude")] ProductViewModel model)
         {
             if (ModelState.IsValid)
             {
+                Product product = new Product
+                {
+                    Name = model.Name,
+                    TariffId = model.TariffId,
+                    ScooterId = model.ScooterId,
+                    StatusId = 1
+                };
                 _context.Add(product);
+                ProductLocation productLocation = new ProductLocation
+                {
+                    Latitude = model.Latitude,
+                    Longitude = model.Longitude,
+                    Product = product
+                };
+                _context.Add(productLocation);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Management", "Home");
             }
-            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Id", product.ScooterId);
-            ViewData["StatusId"] = new SelectList(_context.Set<Status>(), "Id", "Id", product.StatusId);
-            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Id", product.TariffId);
-            return View(product);
+            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Number", model.ScooterId);
+            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Name", model.TariffId);
+            return View(model);
         }
 
         public async Task<IActionResult> Edit(int? id)
@@ -82,30 +95,52 @@ namespace KickScooterSharing.Controllers
             }
 
             var product = await _context.Products.FindAsync(id);
+
             if (product == null)
             {
                 return NotFound();
             }
-            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Id", product.ScooterId);
-            ViewData["StatusId"] = new SelectList(_context.Set<Status>(), "Id", "Id", product.StatusId);
-            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Id", product.TariffId);
-            return View(product);
+
+            var location = await _context.ProductLocations.Where(s => s.Id == product.Id).FirstOrDefaultAsync();
+            ProductViewModel model = new ProductViewModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                TariffId = product.TariffId,
+                ScooterId = product.ScooterId,
+                Latitude = location.Latitude,
+                Longitude = location.Longitude
+            };
+            
+            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Number", product.ScooterId);
+            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Name", product.TariffId);
+            return View(model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,TariffId,ScooterId,StatusId")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,TariffId,ScooterId,Latitude,Longitude")] ProductViewModel model)
         {
-            if (id != product.Id)
+            if (id != model.Id)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
+                Product product = _context.Products.Find(id);
+                ProductLocation productLocation = _context.ProductLocations.Where(l => l.ProductId == product.Id).FirstOrDefault();
+
+                product.Name = model.Name;
+                product.TariffId = model.TariffId;
+                product.ScooterId = model.ScooterId;
+                productLocation.Latitude = model.Latitude;
+                productLocation.Longitude = model.Longitude;
+
                 try
                 {
                     _context.Update(product);
+                    _context.Update(productLocation);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -119,12 +154,11 @@ namespace KickScooterSharing.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Menagement", "Home");
             }
-            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Id", product.ScooterId);
-            ViewData["StatusId"] = new SelectList(_context.Set<Status>(), "Id", "Id", product.StatusId);
-            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Id", product.TariffId);
-            return View(product);
+            ViewData["ScooterId"] = new SelectList(_context.Scooter, "Id", "Number", model.ScooterId);
+            ViewData["TariffId"] = new SelectList(_context.Tariff, "Id", "Name", model.TariffId);
+            return View(model);
         }
 
         public async Task<IActionResult> Delete(int? id)
---
+++ KickScooterSharing/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KickScooterSharing.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int? TariffId { get; set; }
+
+        [Required]
+        public int? ScooterId { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+    }
+}
